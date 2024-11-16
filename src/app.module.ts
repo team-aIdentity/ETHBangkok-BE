fba233420600440fe216ddc2d8ebaea4f6ef32bb
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2024-11-16 13:53:43 +0700

    .env for secret

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,18 @@ import { UploadModule } from './upload/upload.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { Competition } from './competition/competition.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'root',
-      password: 'aidenteti1234!',
-      database: 'wantti',
+      username: process.env.POSTGRES_DB_USERNAME,
+      password: process.env.POSTGRES_DB_PASSWORD,
+      database: process.env.POSTGRES_DB_NAME,
       entities: [User, Competition],
       synchronize: true,
     }),
