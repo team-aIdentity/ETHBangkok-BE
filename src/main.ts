45aefba9f0dbe287e1c2a82ccb87d77f4bcfdc81
이형진 <ihyeongjin@ihyeongjin-ui-MacBookPro.local>
2024-11-16 18:06:20 +0700

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
   }
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
